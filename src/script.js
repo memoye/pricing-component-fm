1b495cca3067d0d0906afaf84efa23e718c2a0fe
Brown Memoye <memoyebrown@gmail.com>
2023-10-10 17:21:31 +0100

    done

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// grab dynamic elements   
 const slider = document.querySelector('.slider')
 let sliderValue = slider.value
 let initialProgress = (sliderValue / slider.max) * 100
@@ -10,55 +11,7 @@ let viewsAmount = document.querySelector('#viewsAmt'),
 let applyDiscount = false
 let currentPrice = price.innerText
 
-// initial state of the slider
-slider.style.background = `linear-gradient(to right, 
-    var(--soft-cyan) ${initialProgress}%, 
-    var(--lighter-grayish-blue) ${initialProgress}%)`
-
-function updateRangePrgress(val) {
-    sliderValue = val
-    const progress = (sliderValue / slider.max) * 100
-
-    slider.style.background = `linear-gradient(to right, 
-        var(--soft-cyan) ${progress}%, 
-        var(--lighter-grayish-blue) ${progress}%)`
-}
-
-slider.addEventListener('input', (e) => {
-    updateRangePrgress(e.target.value)
-})
-
-
-
-function toggleDiscount(val) {
-    if (applyDiscount === false) {
-        toggleHandle.classList.add('toggled')
-        applyDiscount = true
-        price.innerText = val * 0.75
-        return val * 0.75
-    } else {
-        toggleHandle.classList.remove('toggled')
-        applyDiscount = false
-        price.innerText = val
-        return val
-    }
-}
-
-// toggleHandle.classList.add('toggled')
-
-// - 10K pageviews / $8 per month
-// - 50K pageviews / $12 per month
-// - 100K pageviews / $16 per month
-// - 500k pageviews / $24 per month
-// - 1M pageviews / $36 per month
-const data = [
-    { value: 1, pageViews: '10k', price: 8 },
-    { value: 5, pageViews: '50k', price: 12 },
-    { value: 10, pageViews: '100k', price: 16 },
-    { value: 50, pageViews: '500k', price: 24 },
-    { value: 100, pageViews: '1m', price: 36 }
-]
-
+// class for constructing subscription plan variations
 class Plan {
     constructor({ value, pageViews, price }) {
         this.value = value
@@ -72,29 +25,81 @@ class Plan {
     }
 }
 
-const plans = data.map(plan => new Plan({ ...plan }))
+// provided data
+const data = [
+    { value: 1, pageViews: '10k', price: 8 },
+    { value: 5, pageViews: '50k', price: 12 },
+    { value: 10, pageViews: '100k', price: 16 },
+    { value: 50, pageViews: '500k', price: 24 },
+    { value: 100, pageViews: '1m', price: 36 }
+]
 
-toggle.addEventListener('click', () => {
+// initial state of the slider
+slider.style.background = `linear-gradient(to right, 
+    var(--soft-cyan) ${initialProgress}%, 
+    var(--lighter-grayish-blue) ${initialProgress}%)`
 
-    // if (applyDiscount === false) {
-    //     toggleHandle.classList.add('toggled')
-    //     applyDiscount = true
-    // } else {
-    //     toggleHandle.classList.remove('toggled')
-    //     applyDiscount = false
-    // }
-    toggleDiscount(Number(currentPrice))
+// handle slider behavior
+function updateRangePrgress(val) {
+    sliderValue = val
+    const progress = (sliderValue / slider.max) * 100
 
-})
+    slider.style.background = `linear-gradient(to right, 
+        var(--soft-cyan) ${progress}%, 
+        var(--lighter-grayish-blue) ${progress}%)`
+}
 
-slider.addEventListener('input', (e) => {
+// handle discount toggle and update ui with current price
+function toggleDiscount(val) {
+    if (applyDiscount === false) {
+        toggleHandle.classList.add('toggled')
+        applyDiscount = true
+        price.innerText = (val * 0.75).toFixed(2)
+        // return val * 0.75
+    } else {
+        toggleHandle.classList.remove('toggled')
+        applyDiscount = false
+        price.innerText = (val).toFixed(2)
+        // return val
+    }
+}
+
+// handle plan selection and update ui with current plan
+function updatePlan(e) {
     let value = e.target.value
+    viewsAmount.innerText = numFormatter(value * 10000)
 
     for (const plan of plans) {
-        if (plan.value == value) {
-            viewsAmount.innerText = plan.pageViews
-            price.innerText = applyDiscount ? plan.price * 0.75 : plan.price
+        if (plan.value <= value) {
+            price.innerText = applyDiscount ? (plan.price * 0.75).toFixed(2) : (plan.price).toFixed(2)
             currentPrice = plan.price
         }
     }
+}
+
+// converts number to string representation with K and M.
+function numFormatter(num) {
+    if (num >= 1000000) {
+        const result = (num / 1000000).toFixed(1);
+        return result.endsWith('.0') ? result.slice(0, -2) + 'M' : result + 'M';
+    } else if (num >= 1000) {
+        const result = (num / 1000).toFixed(1);
+        return result.endsWith('.0') ? result.slice(0, -2) + 'K' : result + 'K';
+    } else {
+        return num.toString();
+    }
+}
+
+
+
+
+const plans = data.map(plan => new Plan({ ...plan }))
+
+toggle.addEventListener('click', () => {
+    toggleDiscount(Number(currentPrice))
+})
+
+slider.addEventListener('input', e => {
+    updateRangePrgress(e.target.value)
+    updatePlan(e)
 })
